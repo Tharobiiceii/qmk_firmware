5f7ce71969089785ff71dad590cf1dba9654463f
Tharobiiceii <tharobiiceii@gmail.com>
2020-02-27 22:15:48 -0800

    updates

--- keyboards/acheron/shark/keymaps/tharobiiceii/rules.mk
+++ keyboards/acheron/shark/keymaps/tharobiiceii/rules.mk
@@ -7,18 +7,20 @@ CONSOLE_ENABLE = no # Console for debug(+400)
 COMMAND_ENABLE = no # Commands for debug
 SLEEP_LED_ENABLE = no # LED breathing
 NKRO_ENABLE = yes # USB N-Key Rollover
-BACKLIGHT_ENABLE = yes # Backlight
-RGBLIGHT_ENABLE = yes # WS2812 RGB underlight
+BACKLIGHT_ENABLE = no # Backlight
+RGBLIGHT_ENABLE = no # RGB underlight
 MIDI_ENABLE = no # MIDI controls
 UNICODE_ENABLE = no # Unicode
 UNICODEMAP_ENABLE = no # More Unicode
 BLUETOOTH_ENABLE = no # Bluetooth with the Adafruit EZ-Key HID
-AUDIO_ENABLE = no # Audio output
+AUDIO_ENABLE = yes # Audio output
 FAUXCLICKY_ENABLE = no # Buzzer to emulate clicky switches
 API_SYSEX_ENABLE = no  # Quantum SYSEX API
 KEY_LOCK_ENABLE = no # Key lock
 TAP_DANCE_ENABLE = yes # Tap Dance
+RGB_MATRIX_ENABLE = no # RGB Matrix
 
-ifndef QUANTUM_DIR
-    include ../../../../Makefile
-endif
+# ifndef QUANTUM_DIR
+#     include ../../../../Makefile
+# endif
+
